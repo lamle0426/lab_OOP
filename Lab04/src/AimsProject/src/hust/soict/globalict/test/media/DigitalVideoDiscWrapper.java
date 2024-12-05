0d65f59cfcf1260d61e0b6f180dc3b61e590fe01
lamle0426 <lelamvp8@gmail.com>
2024-12-05 23:24:45 +0700

    Lab04

---
+++ Lab04/src/AimsProject/src/hust/soict/globalict/test/media/DigitalVideoDiscWrapper.java
@@ -0,0 +1,12 @@
+package AimsProject.src.hust.soict.globalict.test.media;
+
+import AimsProject.src.hust.soict.globalict.aims.media.DigitalVideoDisc;
+
+public class DigitalVideoDiscWrapper {
+    DigitalVideoDisc dvd;
+    DigitalVideoDiscWrapper(DigitalVideoDisc dvd) {
+        super();
+
+        this.dvd = dvd;
+    }
+}
---
+++ Lab04/src/OtherProjects/hust/soict/globalict/Lab01/src/FirstDialog.java
@@ -0,0 +1,11 @@
+// Lab 01: Environment Setup and Java Basics
+// Ex 2.2: Write, compile the first dialog Java program
+
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0);
+    }
+}
